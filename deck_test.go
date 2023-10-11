5336f250f094222456dc1587ebd62d7545708071
Fernando Gomez <fernando@gomezh.dev>
2023-10-11 19:52:38 -0400

    chapter 36

---
+++ deck_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	expectedLength := 52
+
+	if len(d) != expectedLength {
+		t.Errorf("Expected deck length of %v but got %v", expectedLength, len(d))
+	}
+}
