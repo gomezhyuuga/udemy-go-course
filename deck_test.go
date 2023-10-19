b859f247fb36e04aa3131d0bc408776b8645d30d
Fernando Gomez <fernando@gomezh.dev>
2023-10-19 00:26:21 -0400

    chapter 38: tests for saving file

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -10,4 +13,23 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != expectedLength {
 		t.Errorf("Expected deck length of %v but got %v", expectedLength, len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	d := newDeck()
+	testFilename := "_decktesting"
+
+	// Remove old files
+	os.Remove(testFilename)
+
+	d.saveToFile(testFilename)
+
+	d2 := newDeckFromFile(testFilename)
+
+	if len(d2) != 52 {
+		t.Errorf("File contained a different deck, got %v cards", len(d2))
+	}
+
+	// Remove old files
+	os.Remove(testFilename)
 }
